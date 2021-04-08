be221c5c458f32f4eaeeba31d8342897cee652d6
schottflo <florian.schottmann@gmx.de>
2021-04-08 11:49:23 +0200

    incorporated Ran's feedback

--- num_matching_subtrees.py
+++ num_matching_subtrees.py
@@ -1,27 +1,5 @@
 import nltk
 
-"""
-Brute force idea for testing (probably put in new file):
-Given a tree t, we want to create all possible subtrees of t.
-
-Base case: if t is a pre-terminal, return t
-
-Recursive case: then t has two children, t1 and t2.
-subtrees = []
-subtrees1 = Find all subtrees of t1
-subtrees2 = Find all subtrees of t2
-subtrees.extend(subtrees1)
-subtrees.extend(subtrees2)
-For each s1 of subtrees1:
-    subtrees.append(new_tree(t.head, s1, t2.head))
-For each s2 of subtrees2:
-    subtrees.append(new_tree(t.head, t1.head, s2))
-For each s1 of subtrees1:
-    For each subtree s2 of subtrees2:
-        subtrees.append(new_tree(t.head, t1, t2))
-"""
-
-
 def compute_num_matching_subtrees(t1, t2):
     """
     Given two trees (in CNF) returns the number of matching subtrees.
@@ -41,12 +19,14 @@ def compute_num_matching_subtrees(t1, t2):
 
     # Initialize the DP table (still a dictionary)
     DP_table = {prod: 0 for prod in set(prods_1 + prods_2)}
+    counts = DP_table.copy()
 
     for prod_1 in prods_1:
-        for prod_2 in prods_2:
-            # TODO (RAN): You can move the prod_1.is_lexical() to before the prod_2 loop to save some time
-            if prod_1.is_lexical() and prod_1 == prod_2:
-                DP_table[prod_1] = 1
+        if prod_1.is_lexical():
+            for prod_2 in prods_2:
+                if prod_1 == prod_2:
+                    DP_table[prod_1] = 1
+                    counts[prod_1] += 1
 
     # Algorithm
     for pos1 in t1.treepositions(order="postorder"):
@@ -61,7 +41,6 @@ def compute_num_matching_subtrees(t1, t2):
 
                     if prod_1 == prod_2:
 
-                        # TODO (RAN): What is prev_prod?
                         prev_prod_1_1 = subtree1[(0,)].productions()[0]
                         prev_prod_1_2 = subtree1[(1,)].productions()[0]
 
@@ -82,19 +61,12 @@ def compute_num_matching_subtrees(t1, t2):
                             DP_table[prod_1] += 1
 
     # Accounting for terminal productions that occur more than once
-    # TODO (RAN): You can fit this into the for loop at the start, also, why do we not initialize them with the counts?
-    counts = {prod: 0 for prod in set(prods_1 + prods_2)}
-    for prod_1 in prods_1:
-        for prod_2 in prods_2:
-            if prod_1.is_lexical() and prod_1 == prod_2:
-                counts[prod_1] += 1
-
     for key, value in counts.items():
         if value > 1:
             DP_table[key] *= value
 
     print(DP_table)
-    return(sum(DP_table.values()))
+    return sum(DP_table.values())
 
 
 if __name__ == "__main__":
